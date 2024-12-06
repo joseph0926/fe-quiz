eca87f3b018d3b2e451099d9bbff60cfc0420fc6
joseph0926 <joseph0926@naver.com>
2024-12-07 08:57:20 +0900

    reset

--- src/app/(root)/mentor/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MentorSchedule } from '@/components/scheduling/mentor-schedule';
-
-export default function Mentor() {
-  return (
-    <div className="w-full space-y-6 px-4 py-6 sm:px-6 sm:py-8 lg:px-8">
-      <MentorSchedule />
-    </div>
-  );
-}
